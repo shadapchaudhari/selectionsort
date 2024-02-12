029e3f46367f3b81c47b91ca6603c3cb6626ed56
shadap <shadapchaudhari11@gmail.com>
2024-02-12 18:01:06 +0530

    changefile

--- taskclass.cpp
+++ taskclass.cpp
@@ -9,7 +9,7 @@ public:
         // Bubble sort code
         for (int i = 0; i < arr.size(); i++)
         {
-            for (int j = i + 1; j < arr.size(); j++)
+            for (int j =i + 1; j < arr.size(); j++)
             {
                 if (arr[i] > arr[j])
                 {
